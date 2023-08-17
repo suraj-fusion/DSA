cf1dc2ecc2d1229c49c1bf63d9c4ea3824b9ae80
Suraj Raj <79706268+suraj-fusion@users.noreply.github.com>
2023-08-17 11:59:12 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (96.66%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,77 +10,40 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
-        ListNode temp1=list1;
-        ListNode temp2=list2;
-        ListNode head=null;
-        ListNode temp=head;
-        while(temp1!=null && temp2!=null)
+        if(list1==null && list2==null) return null;
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        ListNode l1;
+        ListNode l2;
+        ListNode res;
+        if(list1.val<=list2.val)
         {
-             if(temp1.val<=temp2.val)
-             {
-                  if(head==null)
-                  {
-                      head=new ListNode(temp1.val);
-                      temp=head;
-                  }
-                  else
-                  {
-                      temp.next=new ListNode(temp1.val);
-                      temp=temp.next;
-                  }
-                  temp1=temp1.next;
-             }
-             else
-             {
-                     if(head==null)
-                  {
-                      head=new ListNode(temp2.val);
-                      temp=head;
-                  }
-                  else
-                  {
-                      temp.next=new ListNode(temp2.val);
-                      temp=temp.next;
-                  }
-                  temp2=temp2.next;
-             }
+            l1=list1;
+            l2=list2;
+        }
+        else
+        {
+            l1=list2;
+            l2=list1;
+        }
+        res=l1;
 
+        while(l1!=null && l2!=null)
+        {
+            ListNode temp=null;
+            while(l1!=null && l1.val<=l2.val)
+            {
+                temp=l1;
+                l1=l1.next;
+            }
 
-             
+            if(temp!=null)
+             temp.next=l2;
+            ListNode swap=l1;
+            l1=l2;
+            l2=swap;
         }
 
-        while( temp1!=null )
-        {
-              if(head==null)
-                  {
-                      head=new ListNode(temp1.val);
-                      temp=head;
-                  }
-                  else
-                  {
-                      temp.next=new ListNode(temp1.val);
-                      temp=temp.next;
-                  }
-
-                   temp1=temp1.next;
-        }
-
-        while(temp2!=null )
-        {
-                 if(head==null)
-                  {
-                      head=new ListNode(temp2.val);
-                      temp=head;
-                  }
-                  else
-                  {
-                      temp.next=new ListNode(temp2.val);
-                      temp=temp.next;
-                  }
-
-                   temp2=temp2.next;
-        }
-        return head;
+        return res;
     }
 }
