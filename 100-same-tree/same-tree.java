c8323a10eaceb92fba96344736e98e9f1521fbbb
Suraj Raj <79706268+suraj-fusion@users.noreply.github.com>
2023-08-18 13:00:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (9.04%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -16,40 +16,12 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
-              ArrayList<Integer> p1=new ArrayList<Integer>();
-              ArrayList<Integer> p2=new ArrayList<Integer>();
-              p1=preorder(p,p1);
-              p2=preorder(q,p2);
-
-             
-             
-             if(p1.size()!=p2.size())
-             {
-                 return false;
-             }
-            for(int i=0;i<p1.size();i++)
+            if(p==null || q==null)
             {
-                     if(!Objects.equals(p1.get(i),p2.get(i)))
-                     {
-                         return false;
-                     }  
+                return p==q;
             }
 
-           return true;
-        
-    }
-
-    ArrayList<Integer>  preorder(TreeNode root,ArrayList<Integer> ans)
-    {
-        if(root==null)
-        {
-            ans.add(null);
-            return ans;
-        }
-        
-        ans.add(root.val);
-        preorder(root.left,ans);
-        preorder(root.right,ans);
-        return ans;
+            return p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+       
     }
 }
