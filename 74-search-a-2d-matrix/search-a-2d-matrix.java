856ae58343720d3c0953d5d2d0457033125c008b
Suraj Raj <79706268+suraj-fusion@users.noreply.github.com>
2023-08-19 12:56:37 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (45.82%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -17,13 +17,25 @@ class Solution {
           return false;
       }
     
-      for(int i=0;i<matrix[row].length;i++)
-      {
-          if(target == matrix[row][i])
-          {
-              return true;
-          }
-      }
-      return false;
+     int low=0;
+     int high=matrix[row].length-1;
+     while(low<=high)
+     {
+         int mid=(low+high)/2;
+         if(matrix[row][mid]==target)
+         {
+             return true;
+         }
+         else if(target > matrix[row][mid])
+         {
+             low=mid+1;
+         }
+         else
+         {
+            high=mid-1;
+         }
+
+     }
+     return false;
 }
 }
